b6bea023e596fbbba564bfc0b8a909d7d423c7d4
Robin Swenson-Healey <rjswenson@gmail.com>
2013-07-23 13:08:42 -0700

    nearest larger // no repeats

--- coding-test-2/practice-problems/lib/00_nearest_larger.rb
+++ coding-test-2/practice-problems/lib/00_nearest_larger.rb
@@ -1,2 +1,21 @@
+
+
 def nearest_larger(arr, idx)
+  num_of_index = arr[idx]       #num value of given index location
+  greater_idx_arr = Array.new   #holds idx's w/ num val > num_of_index
+
+  for x in 0..arr.length-1      
+    if arr[x] > num_of_index
+      greater_idx_arr.push(x)   #pushes all idx fitting perams to array
+    end
+  end
+
+  return nil if greater_idx_arr.empty?     #if none > given, return nil      
+  (greater_idx_arr.length-1).times {greater_idx_arr.pop}  #pop from right to left, leaving leftmost only
+  return greater_idx_arr.pop.to_i  #return last poped out idx number
+  
 end
+  
+
+
+
--- coding-test-2/practice-problems/lib/01_no_repeats.rb
+++ coding-test-2/practice-problems/lib/01_no_repeats.rb
@@ -1,2 +1,18 @@
+
+
 def no_repeats(year_start, year_end)
+  non_repeat_years = Array.new
+  for yr in year_start..year_end  #for each year w/in range
+    non_repeat_years.push(yr) if no_repeat?(yr) == true   #adds to array
+  end
+  return non_repeat_years         #returns array
 end
+
+
+def no_repeat?(year)
+  split_array = year.to_s.split(//)   #splits num into array
+  unique_array = split_array.uniq     #checks to see if array has unique values
+  return true if unique_array == split_array
+  return false
+end
+
