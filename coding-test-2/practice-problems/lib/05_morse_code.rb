f5e904154c249155a384b20c8564cc431ed73527
Robin Swenson-Healey <rjswenson@gmail.com>
2013-07-24 11:59:38 -0700

    morse code//word unscrambler added

--- coding-test-2/practice-problems/lib/05_morse_code.rb
+++ coding-test-2/practice-problems/lib/05_morse_code.rb
@@ -1,2 +1,22 @@
+
 def morse_encode(str)
+  coded_letters_hash = { "a" => ".-", "b" => "-...", "c" => "-.-.",
+                        "d" => "-..", "e" => ".", "f" => "..-.",
+                         "g" => "--.", "h" => "....", "i" => "..",
+                         "j" => ".---", "k" => "-.-", "l" => ".-..",
+                         "m" => "--", "n" => "-.", "o" => "---",
+                         "p" => ".--.", "q" => "--.-", "r" => ".-.",
+                         "s" => "...", "t" => "-", "u" => "..-",
+                         "v" => "...-", "w" => ".--", "x" => "-..-",
+                         "y" => "-.--", "z" => "--.."}
+
+  word_split = str.split(//)    #splits str into array of letters, includes space
+  morse_arr = []
+     
+
+  word_split.each { |ltr| morse_arr.push(coded_letters_hash[ltr])}  #pushes value of letter key to new array
+  morse_arr = morse_arr.join(" ")  #joins array into a string, nil adds +1 space between words
+   
+  return morse_arr
 end
+
--- coding-test-2/practice-problems/lib/06_word_unscrambler.rb
+++ coding-test-2/practice-problems/lib/06_word_unscrambler.rb
@@ -1,2 +1,23 @@
+
 def word_unscrambler(str, words)
+  #split both the word and the dictionary into str arrays
+  #include? sorted string within sorted dictionary
+  #if yes, output array containing said dictionary words
+  #else return empty array
+
+  str_array = str.split(//)
+  sorted_str = str_array.sort
+  
+
+  dictionary_array = words.collect { |wd| wd.split(//)}
+  sorted_dict = dictionary_array.collect {|lt| lt.sort}
+  matched_words = []
+
+  for i in 0..sorted_dict.length-1
+    if sorted_dict[i] == sorted_str
+      matched_words.push(words[i])
+    end
+  end
+  
+  return matched_words
 end
